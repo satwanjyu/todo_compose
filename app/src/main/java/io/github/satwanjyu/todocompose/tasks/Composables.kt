de5b35fda340ed024ddde76111f286e38b30be9e
失魂魚 <satwanjyu@outlook.com>
2023-07-12 15:48:23 +0800

    Implement keyboard action for EditTaskForm

--- app/src/main/java/io/github/satwanjyu/todocompose/tasks/Composables.kt
+++ app/src/main/java/io/github/satwanjyu/todocompose/tasks/Composables.kt
@@ -26,6 +26,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
@@ -53,10 +55,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
@@ -103,7 +110,7 @@ private fun TasksCompact(viewModel: TasksViewModel = viewModel()) {
     AnimatedContent(
         targetState = uiState,
         transitionSpec = {
-            if (uiStateType(uiState) == 0) {
+            if (uiStateType(initialState) == 0 && uiStateType(targetState) == 0) {
                 EnterTransition.None togetherWith ExitTransition.None
             } else {
                 (fadeIn(animationSpec = tween(220, delayMillis = 90)) +
@@ -726,6 +733,13 @@ private fun EditTaskForm(
     notes: String,
     onNotesChange: (String) -> Unit,
 ) {
+    var titleValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(title))
+    }
+    var notesValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(notes))
+    }
+
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier.fillMaxHeight(),
@@ -733,21 +747,47 @@ private fun EditTaskForm(
         val textFieldModifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp)
+
         item {
             TextField(
-                value = title,
-                onValueChange = onTitleChange,
+                value = titleValue,
+                onValueChange = {
+                    titleValue = it
+                    onTitleChange(it.text)
+                },
                 modifier = textFieldModifier,
                 label = { Text(stringResource(R.string.title)) },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        defaultKeyboardAction(ImeAction.Next)
+                        notesValue = notesValue.copy(
+                            selection = TextRange(notesValue.text.length)
+                        )
+                    }
+                )
             )
         }
         item {
             TextField(
-                value = notes,
-                onValueChange = onNotesChange,
+                value = notesValue,
+                onValueChange = {
+                    notesValue = it
+                    onNotesChange(it.text)
+                },
                 modifier = textFieldModifier,
                 label = { Text(stringResource(R.string.notes)) },
                 minLines = 6,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        defaultKeyboardAction(ImeAction.Done)
+                    }
+                ),
             )
         }
     }
